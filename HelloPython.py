5a1262b915f49f98eb0fee4466618229f655b551
finleyl <finley.lewis@mycit.ie>
2018-12-03 09:18:00 +0000

    Fourth commit to jenkins

--- HelloPython.py
+++ HelloPython.py
@@ -1,5 +1,5 @@
 import datetime
-print("Hello, here is the current date and time!")
+print("Hello, this is the current date and time!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
